1e57dd5a94cb18745a251abb14a576120b22225a
ahmetbengoz <ahmetbengoz.lu@gmail.com>
2026-01-21 11:36:44 +0100

    Add core algorithms

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,18 @@
+import numpy as np
+
+def tour_length(tour, D):
+    """
+    Computes total tour length for a given permutation.
+    """
+    n = len(tour)
+    return sum(D[tour[i], tour[(i + 1) % n]] for i in range(n))
+
+
+def random_tour(n):
+    """
+    Generates a random TSP tour.
+    """
+    tour = np.arange(n)
+    np.random.shuffle(tour)
+    return tour
 
